5323f1d944a9b85431e586dde801c7bcb359e425
theboxboy1 <126028130+theboxboy1@users.noreply.github.com>
2023-10-10 16:53:41 -0400

    Create qt1.java

---
+++ qt1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+    
+        System.out.print("Input a name: ");
+    
+        String name = input.nextLine();
+
+        if (name.toLowerCase().equals("zakariya")) {
+            System.out.println("Same name as programmer!");
+        } 
+        else {
+            System.out.print("Not the same name as programmer!");
+        }
+    
+    }
+}
