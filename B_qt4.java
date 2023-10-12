3a9217482f20b0fd37513bd9be8f276c4903ae61
theboxboy1 <126028130+theboxboy1@users.noreply.github.com>
2023-10-11 21:42:09 -0400

    Create B_qt4.java

---
+++ B_qt4.java
@@ -0,0 +1,17 @@
+
+public class App {
+    public static void main(String[] args) {
+       
+        double num1 = 3.0; double num2 = 5.0; double num3 = 4.0;
+
+        if (num2>num1 && num2>num3){
+            System.out.println(num2 + " is the largest number");
+        }
+        else if (num1>num2 && num1>num3){
+            System.out.println(num1 + " is the largest number");
+        }
+        else{
+            System.out.println(num3 + " is the largest number");
+        }
+    }
+}
