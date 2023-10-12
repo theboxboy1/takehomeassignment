038b61d8674a79f5f49bb8ae3f99f91b92033897
theboxboy1 <126028130+theboxboy1@users.noreply.github.com>
2023-10-12 13:19:42 -0400

    Update A_qt1.java

--- A_qt1.java
+++ A_qt1.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in); //makes input a Scanner class
     
-        System.out.print("Input a name: ");
+        System.out.print("Input a name: "); // prints out "Input a name: " 
     
-        String name = input.nextLine();
+        String name = input.nextLine(); //stores the input
 
         if (name.toLowerCase().equals("zakariya")) {
-            System.out.println("Same name as programmer!");
+            System.out.println("Same name as programmer!"); // if the input equals to "zakariya ", it will print that it is the same name as programmer
         } 
         else {
-            System.out.print("Not the same name as programmer!");
+            System.out.print("Not the same name as programmer!"); // if input is not my name, the program will print the statement in quotes
         }
         input.close();
     }
