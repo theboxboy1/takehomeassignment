c4a3527c9b092510c255f359e0129f3f99240240
theboxboy1 <126028130+theboxboy1@users.noreply.github.com>
2023-10-14 11:52:29 -0400

    Update B_qt6.java

--- B_qt6.java
+++ B_qt6.java
@@ -1,35 +1,35 @@
-import java.util.Scanner;
+import java.util.Scanner; // imports scanner
 
 public class App {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in); // creates scanner class
 
-        System.out.print("Input language preference: ");
-        String language = input.nextLine();
+        System.out.print("Input language preference: "); // prints out statement asking user to input language
+        String language = input.nextLine(); // ties variable to user string input
 
-        switch (language.toLowerCase()) {
-            case "english":
+        switch (language.toLowerCase()) { // creates switch class for language variable and lowercases user input in case they capitalize their input
+            case "english": // if the user inputs english, will print out english greetings
                 System.out.print(
                     "Hello! How's it going?"
                 );
                 break;
 
-            case "german":
+            case "german": // prints german greeting if user inputs german
                 System.out.print(
                     "Hi, wie geht's?"
                 );
                 break;
 
-            case "french":
+            case "french": // prints french greeting if user inputs french
                 System.out.print(
                     "Bonjour! comment ça va?"
                 );
                 break;
 
-            default:
+            default: // if the user does not input any of the above languages, will output that it does not know any of the user's inputted language greetings
                 System.out.println("I don't know any " + language + " greetings.");
         }
 
-        input.close();
+        input.close(); // stops taking input from user
     }
 }
