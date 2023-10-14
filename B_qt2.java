bcb7ec1830af0f8488cdbe6b18b0ed67d16e6002
theboxboy1 <126028130+theboxboy1@users.noreply.github.com>
2023-10-14 11:32:19 -0400

    Update B_qt2.java

--- B_qt2.java
+++ B_qt2.java
@@ -2,15 +2,15 @@
 public class App {
     public static void main(String[] args) {
        
-        double num1 = 3.0; double num2 = 5.0; double num3 = 4.0;
+        double num1 = 3.0; double num2 = 5.0; double num3 = 4.0; // initializing variables
 
-        if (num2<num1 && num2<num3){
+        if (num2<num1 && num2<num3){ // if num2<num1 and num2<num3, num2 is therefore the smallest number and is printed as such
             System.out.println(num2 + " is the smallest number");
         }
-        else if (num1<num2 && num1<num3){
+        else if (num1<num2 && num1<num3){ // if num1<num1 and num1<num2, num1 is therefore the smallest number and is printed as such
             System.out.println(num1 + " is the smallest number");
         }
-        else{
+        else{ // If none of the top  conditions apply, then logically num3 must be the smallest number 
             System.out.println(num3 + " is the smallest number");
         }
     }
